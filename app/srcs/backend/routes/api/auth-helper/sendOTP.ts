a6df2af9b8351ce17187a3b7df1c4b8219036e77
Natsuha <ethanyap8@gmail.com>
2025-09-04 03:23:53 +0800

    OTP email format parse harder

--- app/srcs/backend/routes/api/auth-helper/sendOTP.ts
+++ app/srcs/backend/routes/api/auth-helper/sendOTP.ts
@@ -1,16 +1,42 @@
 import nodemailer from 'nodemailer';
+import crypto from 'crypto';
+import validator from 'validator';
+import dns from 'dns/promises';
+
 import { SMTP_EMAIL, SMTP_APP_SECRET } from "../../../server.ts";
 import { otpTemp } from '../auth-api.ts';
 
 export async function sendOTP(email: string): Promise<boolean>
 {
-	const otp = (Math.floor(Math.random() * 1000000)).toString().padStart(6, '0');
+	if (!validator.isEmail(email))
+	{
+		console.error("Error: Invalid email format: ", email);
+		return (false);
+	}
+
+	try
+	{
+		const domain = email.split("@")[1];
+		const mx = await dns.resolveMx(domain);
+		if (!mx || mx.length === 0)
+		{
+			console.error("Error: No MX records for domain: ", domain);
+			return (false);
+		}
+	}
+	catch (error)
+	{
+		console.error("Error: MX resolve failed: ", email, error);
+		return (false);
+	}
+
+	const otp = crypto.randomInt(0, 1000000).toString().padStart(6, "0");
 	otpTemp[email] = { otp, expires: Date.now() + 5 * 60 * 1000, attempts: 0 };
 
 	try
 	{
 		const transporter = nodemailer.createTransport({
-			host: 'smtp.gmail.com',
+			host: `"ft_klbq 2FA Verification" <${SMTP_EMAIL}>`,
 			port: 587,
 			secure: false,
 			auth: {
