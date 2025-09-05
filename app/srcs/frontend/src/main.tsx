fc6d0f3a74a29de3aa1185a62087ba83a7583f86
yabi <hin050924@gmail.com>
2025-09-06 04:34:56 +0800

    react env set up

---
+++ app/srcs/frontend/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./input.css";
+
+function App() {
+  return <h1 className="text-3xl font-bold">Hello React + Tailwind!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
