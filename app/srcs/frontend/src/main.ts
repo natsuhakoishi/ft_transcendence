ad09bd278d70dfe6586f89ece96eece937ed5b20
yabi <hin050924@gmail.com>
2025-09-05 21:05:55 +0800

    set up tailwind css

--- app/srcs/frontend/src/main.ts
+++ app/srcs/frontend/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import '.src/input.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
