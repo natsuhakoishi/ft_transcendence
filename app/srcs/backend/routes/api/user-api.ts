d343a6d7a197d325bbc19ad7b26741d008aa8cc0
Natsuha <ethanyap8@gmail.com>
2025-09-07 20:53:02 +0800

    user-api syntax fix

--- app/srcs/backend/routes/api/user-api.ts
+++ app/srcs/backend/routes/api/user-api.ts
@@ -1,8 +1,8 @@
 import type { FastifyPluginAsync } from 'fastify';
 import { getUserById } from "../../database/user.ts";
 
-const userApi: FastifyPluginAsync = async (fastify) => {
-	fastify.get('/me', async (req, res) => {
+const userApi: FastifyPluginAsync = async (fastify: any) => {
+	fastify.get('/me', async (req: any, res: any) => {
 		const user_id = req.user;
 		if (!user_id)
 			return res.status(403).send({ message: 'Permission Denied' });
