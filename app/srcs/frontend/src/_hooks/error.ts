4a987e7e1afcfe1692cd069a9f5df33045308953
nightZQ <pandazhen11@gmail.com>
2025-10-15 04:04:49 +0800

    [update: server error handler

--- app/srcs/frontend/src/_hooks/error.ts
+++ app/srcs/frontend/src/_hooks/error.ts
@@ -2,21 +2,24 @@ import React from "react";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useLang } from "./language";
+import { apiFetchPrivate } from "../utils";
 
 let globalErrorHandler: Record<string, (() => void) | null> = {
   "401": null, "503": null, "404": null,
 };
 
 export function useGlobalErrorMonitor() {
+  let timer: any;
   const navigate = useNavigate();
   const { t } = useLang();
 
   React.useEffect(() => {
-
     //Access Token Expired
     globalErrorHandler["401"] = () => {
       toast.error(t("pop.401"));
-      navigate("/auth", { replace: true });
+      timer = setTimeout(() => {
+        navigate("/auth", { replace: true });
+      }, 500);
     };
 
     //Backend Server Down
@@ -24,30 +27,31 @@ export function useGlobalErrorMonitor() {
       toast.error(`${t("pop.503")}`);
       // const currentPath = location.pathname;
 
-      await new Promise(r => setTimeout(r, 1000 * 25));
+      await new Promise(r => setTimeout(r, 1000 * 5));
 
       try {
-        const res = await fetch(`${import.meta.env.VITE_API_PRI_FETCH}me`, { credentials: "include" });
-        if (!res.ok) throw new Error("Still unreachable");
-
+        await apiFetchPrivate("me", { method: "POST", body: "{}" });
         toast.success(t("pop.503-SUCCESS"));
         window.location.reload();
       } catch {
         toast.error(t("pop.503-FAIL"));
-        setTimeout(() => {
+        timer = setTimeout(() => {
           navigate("/auth", { replace: true });
-        }, 2000);
+        }, 500);
       }
     };
 
     //User not found
     globalErrorHandler["404"] = () => {
       toast.error(t("pop.USER-404"));
-      navigate("/auth", { replace: true });
+      timer = setTimeout(() => {
+        navigate("/auth", { replace: true });
+      }, 500);
     };
 
     return () => {
       Object.keys(globalErrorHandler).forEach(k => (globalErrorHandler[k as string] = null));
+      if (timer) clearTimeout(timer);
     };
   }, [navigate, location]);
 }
