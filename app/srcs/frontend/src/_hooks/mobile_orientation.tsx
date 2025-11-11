23601a49c5937b20d6fb133eb4a0452bf609eba9
nightZQ <pandazhen11@gmail.com>
2025-11-12 01:38:12 +0800

    fix: orien

--- app/srcs/frontend/src/_hooks/mobile_orientation.tsx
+++ app/srcs/frontend/src/_hooks/mobile_orientation.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { isMobile } from "../utils";
 
 function useOrientation() {
   const getState = () => {
@@ -18,19 +19,22 @@ function useOrientation() {
 }
 
 export default function OrientationGuard({ children } : { children: React.ReactNode }) {
-  const isPortrait = window.innerHeight > window.innerWidth;
 
+  const { isPortrait } = useOrientation();
   const showOverlay = isPortrait;
 
   return (
     <>
       {showOverlay && (
-        <div className="fixed inset-0 z-[100] flex flex-col justify-center items-center text-white text-center">
-          <img src="/pic/icons/rotate_warn.png" className="w-25 aspect-square object-cover"/>
-          <img src="/pic/icons/rotate_plz.gif" className="w-25 aspect-square object-cover"/>
-          <div className="flex gap-10 items-start mt-2">
-            <img src="/pic/icons/rotate_warn_3.png" className="w-25 aspect-square object-cover"/>
-            <img src="/pic/icons/rotate_warn_2.png" className="w-20 aspect-square object-cover my-10"/>
+        <div className="fixed w-screen h-screen inset-0 z-[100] flex flex-col justify-center items-center text-white">
+          <div className="flex gap-2 items-start mt-2">
+            <img src="/pic/icons/rotate_warn.png" className="w-20 aspect-square object-contain"/>
+            <img src="/pic/icons/rotate_plz.gif" className="w-20 aspect-square object-cover"/>
+            <img src="/pic/icons/rotate_warn_3.png" className="w-17 aspect-square object-cover"/>
+          </div>
+          <div className="flex gap-2 justify-center items-center">
+            <p className="text-xl font-semibold">Rotate your device !</p>
+            <img src="/pic/icons/rotate_warn_2.png" className="w-14 aspect-square object-cover"/>
           </div>
         </div>
       )}
