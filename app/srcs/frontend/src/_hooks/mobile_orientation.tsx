38362010a69837d9d6727679f9ca4625341003ec
nightZQ <pandazhen11@gmail.com>
2025-11-12 01:08:50 +0800

    update: orientation lock -add another two member -

--- app/srcs/frontend/src/_hooks/mobile_orientation.tsx
+++ app/srcs/frontend/src/_hooks/mobile_orientation.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { isMobile } from "../utils";
 
 function useOrientation() {
   const getState = () => {
@@ -19,16 +18,20 @@ function useOrientation() {
 }
 
 export default function OrientationGuard({ children } : { children: React.ReactNode }) {
-  const { isPortrait, isMobile } = useOrientation();
+  const isPortrait = window.innerHeight > window.innerWidth;
 
-  const showOverlay = isMobile && isPortrait;
+  const showOverlay = isPortrait;
 
   return (
     <>
       {showOverlay && (
-        <div className="fixed inset-0 z-[9999] flex flex-col justify-center items-center text-white text-center px-6">
-          <img src="/pic/icons/rotate_warn.png" className="w-20 aspect-square object-cover"/>
-          <p className="text-xl font-semibold">Rotate your device !</p>
+        <div className="fixed inset-0 z-[100] flex flex-col justify-center items-center text-white text-center">
+          <img src="/pic/icons/rotate_warn.png" className="w-25 aspect-square object-cover"/>
+          <img src="/pic/icons/rotate_plz.gif" className="w-25 aspect-square object-cover"/>
+          <div className="flex gap-10 items-start mt-2">
+            <img src="/pic/icons/rotate_warn_3.png" className="w-25 aspect-square object-cover"/>
+            <img src="/pic/icons/rotate_warn_2.png" className="w-20 aspect-square object-cover my-10"/>
+          </div>
         </div>
       )}
 
